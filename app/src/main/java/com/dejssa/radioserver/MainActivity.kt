7630d7cce4f944188e0d501645541c41ce52dbbb
Aliaksei Kapura <aleksmemby@gmail.com>
2022-05-20 12:39:33 +0300

    Start server, play radio

---
+++ app/src/main/java/com/dejssa/radioserver/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.dejssa.radioserver
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import com.dejssa.radioserver.Service.*
+
+class MainActivity : AppCompatActivity() {
+    private var httpServer: WebServer = WebServer()
+    private var serverUp = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        serverButton.setOnClickListener {
+            serverUp = if(!serverUp){
+                startServer()
+                true
+            } else{
+                stopServer()
+                false
+            }
+        }
+    }
+
+    // DON'T FORGET to stop the server
+    override fun onDestroy() {
+        super.onDestroy()
+        httpServer.stop()
+    }
+
+    private fun startServer() {
+        httpServer.pageFile = loadWebPage()
+        httpServer.start()
+        serverTextView.text = getString(R.string.server_up)
+        serverButton.text = getString(R.string.stop_server)
+    }
+
+    private fun stopServer() {
+        if (httpServer != null) {
+            httpServer.stop();
+            serverTextView.text = getString(R.string.server_down)
+            serverButton.text = getString(R.string.start_server)
+        }
+    }
+
+    private  fun loadWebPage(): String {
+        var webPageContent = ""
+        try {
+            val inputStream = assets.open("index.html")
+            val size: Int = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer).toString()
+            webPageContent = String(buffer)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return webPageContent;
+    }
+}
+
